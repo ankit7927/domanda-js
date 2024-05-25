d5fea45fd7808457519d386586dd0e720b09dc85
ankit prajapati <ap8424856@gmail.com>
2024-05-25 18:11:09 +0530

    added hbs

--- main.js
+++ main.js
@@ -2,26 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./configs/config.db");
+const { engine } = require("express-handlebars");
+const path = require("path")
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, }));
+app.engine('handlebars', engine({extname: ".hbs"}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, "views"));
 
 if (process.env.NODE_ENV === "dev") {
     const morgan = require("morgan");
     app.use(morgan("dev"));
 }
 
-app.use((err, req, res, next) => {
-    const statusCode = err.status || 500;
-    res.status(statusCode).json({ message: err.message })
-});
-
 connectDB()
 
 app.use("/api", require("./routes/api/route.root"))
+app.use("", (req, res)=>{
+    res.render("index.hbs")
+})
 
 mongoose.connection.once("open", () => {
     console.log("connected to database");
