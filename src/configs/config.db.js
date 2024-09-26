7c1db4e22446cdbeba5d183b889f74898a204604
ankit <ap8424856@gmail.com>
2024-09-26 10:38:24 +0530

    fixed sone issues

--- src/configs/config.db.js
+++ src/configs/config.db.js
@@ -1,17 +1,23 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-let db_url = ""
+let db_url = "";
 
-if (process.env.NODE_ENV === "pro") db_url = process.env.REMOTE_DATABASE_URL || "";
+if (process.env.NODE_ENV === "pro")
+	db_url = process.env.REMOTE_DATABASE_URL || "";
 else db_url = "mongodb://127.0.0.1:27017/domanda";
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-  try {
-    await mongoose.connect(db_url)
-  } catch (error) {
-    console.log(error)
-  }
-}
+	try {
+		await mongoose.connect(db_url, {
+			auth: {
+				username: process.env.MONGO_USERNAME,
+				password: process.env.MONGO_PASSWORD,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 module.exports = connectDB;
